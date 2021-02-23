9b2aad8373793d8448e272c1cd796189be890acb
Yvens Vasconcelos <62451203+Harlez@users.noreply.github.com>
2021-02-23 16:41:51 -0300

    Add files via upload

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/Harlez.png" alt="Yvens Vasconcelos"/>
+            <div>
+                <strong>Yvens Vasconcelos</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
